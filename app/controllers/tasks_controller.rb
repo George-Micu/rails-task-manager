4a3561f9b8055170303ff1c1b50e1f406693e6fb
Alex Micu <alex.micu2012@yahoo.com>
2022-10-15 16:31:31 -0400

    fixed the ckeck box

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -34,6 +34,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
